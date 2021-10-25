b6ac41241041ea2850e8edb8dcd26f9587f776b3
Metehan Bolat <metehan.bolat.ie@gmail.com>
2021-10-25 19:32:37 +0300

    Composable created.

--- app/src/main/java/com/metehanbolat/cryptocompose/viewmodel/CryptoDetailViewModel.kt
+++ app/src/main/java/com/metehanbolat/cryptocompose/viewmodel/CryptoDetailViewModel.kt
@@ -1,4 +1,18 @@
 package com.metehanbolat.cryptocompose.viewmodel
 
-class CryptoDetailViewModel {
+import androidx.lifecycle.ViewModel
+import com.metehanbolat.cryptocompose.model.Crypto
+import com.metehanbolat.cryptocompose.repository.CryptoRepository
+import com.metehanbolat.cryptocompose.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CryptoDetailViewModel @Inject constructor(
+    private val repository: CryptoRepository
+) : ViewModel(){
+
+    suspend fun getCrypto(id : String) : Resource<Crypto>{
+        return repository.getCrypto(id)
+    }
 }
